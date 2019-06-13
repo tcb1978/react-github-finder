7cf544aa7c05ce9cca502db124525e738c548796
Matthew Eldredge <mattheweldredge@gmail.com>
2019-06-13 15:13:15 -0700

    get search results with search text

--- src/App.js
+++ src/App.js
@@ -12,16 +12,28 @@ class App extends Component {
     users: [],
     loading: false,
   }
+
   async componentDidMount() {
-    this.setState({
-      loading: true,
-    })
+    // this.setState({
+    //   loading: true,
+    // })
 
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    console.log(res.data);
+    // const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    // console.log(res.data);
+
+    // this.setState({
+    //   users: res.data,
+    //   loading: false,
+    // })
+  }
+
+  // Search GitHub Users
+  searchUsers = async (text) => {
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    console.log(res.data.items);
 
     this.setState({
-      users: res.data,
+      users: res.data.items,
       loading: false,
     })
   }
@@ -31,7 +43,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search/>
+          <Search searchUsers={this.searchUsers}/>
           <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
