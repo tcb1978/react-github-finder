2596befe6b6c6fa6ee6b3ad6d92ce3e3a46ac71c
Matthew Eldredge <mattheweldredge@gmail.com>
2019-06-13 12:17:58 -0700

    fix camel casing

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -7,20 +7,20 @@ class Users extends Component {
       {
         id: '1',
         login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
+        avatarUrl: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        htmlUrl: 'https://github.com/mojombo'
       },
       {
         id: '2',
         login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        avatarUrl: 'https://avatars0.githubusercontent.com/u/1?v=4',
         html_url: 'https://github.com/mojombo'
       },
       {
         id: '3',
         login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
+        avatarUrl: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        htmlUrl: 'https://github.com/mojombo'
       },
     ]
   }
