319bad403f2515b28e655755cab7d31b49b04b7f
Matthew Eldredge <mattheweldredge@gmail.com>
2019-06-13 12:15:48 -0700

    add airbnb config/eslint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar/>
+        <Navbar />
         <div className="container">
           <Users />
         </div>
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react'
+/* eslint-disable react/prop-types */
+import React from 'react';
 
-class UserItem extends Component {
+const UserItem = ({ user: { login, avatarUrl, htmlUrl } }) => (
+  <div className="card text-center">
+    <img src={avatarUrl} alt="" className="round-img" style={{ width: '60px' }} />
+    <h3>{login}</h3>
+    <a href={htmlUrl} className="btn btn-dark btn-sm my-1">More</a>
+  </div>
+);
 
-  render() {
-    const {login, avatar_url, html_url} = this.props.user;
-
-    return (
-      <div className="card text-center">
-        <img src={avatar_url} alt="" className="round-img"  style={{width: '60px'}}/>
-        <h3>{login}</h3>
-        <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-      </div>
-    )
-  }
-}
-
-export default UserItem
+export default UserItem;
