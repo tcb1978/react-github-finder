d2e8c946003fa59805d537f30b291035aa95f35b
tcb1978 <mattheweldredge@gmail.com>
2019-07-04 10:02:53 -0700

    home and not found components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
+import Home from './components/pages/Home';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import './App.css';
 
 import GithubState from './context/github/GithubState';
@@ -21,17 +21,10 @@ const App = () => {
             <div className="container">
               <Alert/>
               <Switch>
-                <Route exact path="/" render={props => (
-                  <Fragment>
-                    <Search/>
-                    <Users />
-                  </Fragment>
-                )}/>
-
+                <Route exact path="/" component={Home}/>
                 <Route exact path="/about" component={About}/>
-
-                <Route exact path="/user/:login" component={User} />
-
+                <Route exact path="/user/:login" component={User}/>
+                <Route component={NotFound}/>
               </Switch>
             </div>
           </div>
@@ -39,6 +32,6 @@ const App = () => {
       </AlertState>
     </GithubState>
   );
-}
+};
 
 export default App;
